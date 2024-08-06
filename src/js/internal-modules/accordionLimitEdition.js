79d0566a23592fbc9f1741e4de00a0e05d3bb6bc
kikeestrada <kikeestradadev@gmail.com>
2024-08-06 12:38:03 -0600

    adjustments

--- src/js/internal-modules/accordionLimitEdition.js
+++ src/js/internal-modules/accordionLimitEdition.js
@@ -9,19 +9,22 @@ const accordionLimitEdition = () => {
         accordions.forEach(accordion => {
             const titleContainer = accordion.querySelector('.accordion-limit-edition__title-container');
             const panel = accordion.querySelector('.accordion-limit-edition__panel');
+            const titleContainerItem2 = accordion.querySelector('.accordion-limit-edition__title-container-item:nth-child(2)');
             const showIcon = accordion.querySelector('.accordion-limit-edition__show-ico');
-			
 
-            if (titleContainer && panel && showIcon) {
+            if (titleContainer && panel && titleContainerItem2 && showIcon) {
                 titleContainer.addEventListener('click', () => {
                     // Cerrar todos los demás artículos
                     accordions.forEach(otherAccordion => {
                         if (otherAccordion !== accordion) {
                             const otherTitleContainer = otherAccordion.querySelector('.accordion-limit-edition__title-container');
                             const otherPanel = otherAccordion.querySelector('.accordion-limit-edition__panel');
+                            const otherTitleContainerItem2 = otherAccordion.querySelector('.accordion-limit-edition__title-container-item:nth-child(2)');
                             const otherShowIcon = otherAccordion.querySelector('.accordion-limit-edition__show-ico');
+
                             otherTitleContainer.classList.remove('active');
                             otherPanel.classList.remove('active');
+                            otherTitleContainerItem2.classList.remove('active');
                             otherShowIcon.classList.remove('active');
                         }
                     });
@@ -29,6 +32,7 @@ const accordionLimitEdition = () => {
                     // Abrir/cerrar el artículo actual
                     titleContainer.classList.toggle('active');
                     panel.classList.toggle('active');
+                    titleContainerItem2.classList.toggle('active');
                     showIcon.classList.toggle('active');
                 });
             }
