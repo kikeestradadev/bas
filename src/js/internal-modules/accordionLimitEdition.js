dac88fe14bfc032b3c0b9a02bd026d07706faab3
kikeestrada <kikeestradadev@gmail.com>
2024-08-06 13:46:02 -0600

    adjustments

--- src/js/internal-modules/accordionLimitEdition.js
+++ src/js/internal-modules/accordionLimitEdition.js
@@ -10,22 +10,26 @@ const accordionLimitEdition = () => {
             const titleContainer = accordion.querySelector('.accordion-limit-edition__title-container');
             const panel = accordion.querySelector('.accordion-limit-edition__panel');
             const titleContainerItem2 = accordion.querySelector('.accordion-limit-edition__title-container-item:nth-child(2)');
-            const showIcon = accordion.querySelector('.accordion-limit-edition__show-ico');
+            const showLabel = accordion.querySelector('.accordion-limit-edition__show-label');
 
-            if (titleContainer && panel && titleContainerItem2 && showIcon) {
+            if (titleContainer && panel && titleContainerItem2 && showLabel) {
                 titleContainer.addEventListener('click', () => {
+                    // Verificar si el elemento actual está activo
+                    const isActive = titleContainer.classList.contains('active');
+
                     // Cerrar todos los demás artículos
                     accordions.forEach(otherAccordion => {
                         if (otherAccordion !== accordion) {
                             const otherTitleContainer = otherAccordion.querySelector('.accordion-limit-edition__title-container');
                             const otherPanel = otherAccordion.querySelector('.accordion-limit-edition__panel');
                             const otherTitleContainerItem2 = otherAccordion.querySelector('.accordion-limit-edition__title-container-item:nth-child(2)');
-                            const otherShowIcon = otherAccordion.querySelector('.accordion-limit-edition__show-ico');
+                            const otherShowLabel = otherAccordion.querySelector('.accordion-limit-edition__show-label');
 
                             otherTitleContainer.classList.remove('active');
                             otherPanel.classList.remove('active');
                             otherTitleContainerItem2.classList.remove('active');
-                            otherShowIcon.classList.remove('active');
+                            otherShowLabel.classList.remove('active');
+                            otherShowLabel.textContent = 'Show More';
                         }
                     });
 
@@ -33,7 +37,8 @@ const accordionLimitEdition = () => {
                     titleContainer.classList.toggle('active');
                     panel.classList.toggle('active');
                     titleContainerItem2.classList.toggle('active');
-                    showIcon.classList.toggle('active');
+                    showLabel.classList.toggle('active');
+                    showLabel.textContent = isActive ? 'Show More' : 'Show Less';
                 });
             }
         });
