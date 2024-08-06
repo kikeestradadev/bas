299f741718a5674b1cb16bd8f887d6f74e9505c3
kikeestrada <kikeestradadev@gmail.com>
2024-08-06 12:28:27 -0600

    adjustments

--- src/js/internal-modules/accordionLimitEdition.js
+++ src/js/internal-modules/accordionLimitEdition.js
@@ -9,22 +9,27 @@ const accordionLimitEdition = () => {
         accordions.forEach(accordion => {
             const titleContainer = accordion.querySelector('.accordion-limit-edition__title-container');
             const panel = accordion.querySelector('.accordion-limit-edition__panel');
+            const showIcon = accordion.querySelector('.accordion-limit-edition__show-ico');
+			
 
-            if (titleContainer && panel) {
+            if (titleContainer && panel && showIcon) {
                 titleContainer.addEventListener('click', () => {
                     // Cerrar todos los demás artículos
                     accordions.forEach(otherAccordion => {
                         if (otherAccordion !== accordion) {
                             const otherTitleContainer = otherAccordion.querySelector('.accordion-limit-edition__title-container');
                             const otherPanel = otherAccordion.querySelector('.accordion-limit-edition__panel');
+                            const otherShowIcon = otherAccordion.querySelector('.accordion-limit-edition__show-ico');
                             otherTitleContainer.classList.remove('active');
                             otherPanel.classList.remove('active');
+                            otherShowIcon.classList.remove('active');
                         }
                     });
 
                     // Abrir/cerrar el artículo actual
                     titleContainer.classList.toggle('active');
                     panel.classList.toggle('active');
+                    showIcon.classList.toggle('active');
                 });
             }
         });
