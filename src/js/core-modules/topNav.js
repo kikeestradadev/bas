93094af095d0cd0378f716683439cfdac8eb978c
kikeestrada <kikeestradadev@gmail.com>
2024-08-08 16:30:09 -0600

    adjustemts

--- src/js/core-modules/topNav.js
+++ src/js/core-modules/topNav.js
@@ -1,21 +1,16 @@
 const topNav = () => {
 	document.addEventListener('DOMContentLoaded', () => {
-		const hamburger = document.querySelector('.top-nav__hamburguer');
-		const menuContainer = document.querySelector('.top-nav__menu-container');
-		const openMenu = document.querySelector('.top-nav__hamburguer-open');
-		const closeMenu = document.querySelector('.top-nav__hamburguer-close');
-		const body = document.body;
-	
-		if (hamburger && menuContainer) {
-			hamburger.addEventListener('click', () => {
-				menuContainer.classList.toggle('active');
-				body.classList.toggle('active');
-				openMenu.classList.toggle('active');
-				closeMenu.classList.toggle('active');
-				console.log('Hamburger menu toggled');
+		const menuItems = document.querySelectorAll('.top-nav__menu-li');
+
+		if (menuItems.length > 0) {
+			menuItems.forEach(item => {
+				const hasSubMenu = item.querySelector('ul');
+				const anchorIcon = item.querySelector('.top-nav__menu-anchor-text-ico');
+
+				if (hasSubMenu && anchorIcon) {
+					anchorIcon.classList.add('active');
+				}
 			});
-		} else {
-			console.error('Hamburger or menu container not found');
 		}
 	});
 };
